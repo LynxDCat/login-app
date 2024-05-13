35a633b14860e1e853ef437704446378c1fafc7d
Jay Ruiz De Ocampo <jayruizdeocampo11503@gmail.com>
2024-05-13 15:11:09 +0800

    Upper section

--- app/index.jsx
+++ app/index.jsx
@@ -1,16 +1,104 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { TextInput, Image, StyleSheet, View, Text } from "react-native";
+import email from "../assets/images/email.png";
+import pass from "../assets/images/pass.png";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Test</Text>
+    <View style={styles.container}>
+      <Text style={styles.logintxt}>Login</Text>
+
+      <Text style={styles.hellotxt}>Hello, welcome back!</Text>
+      <Text style={styles.welcomemsgtxt}>
+        Welcome back please {"\n"}sign in again
+      </Text>
+
+      <View style={styles.emailstyle}>
+        <Image source={email} style={styles.loginimg}></Image>
+        <TextInput
+          placeholderTextColor="#C4C4C4"
+          style={styles.logintxt}
+          placeholder="Email"
+        ></TextInput>
+      </View>
+
+      <View style={styles.passstyle}> 
+        <Image source={pass} style={styles.loginimg}></Image>
+        <TextInput
+          placeholder="Password"
+          placeholderTextColor="#C4C4C4"
+          style={styles.logintxt}
+          secureTextEntry={true}
+        ></TextInput>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: "#1282AD",
+  },
+
+  logintxt: {
+    fontFamily: "Roboto",
+    fontWeight: "bold",
+    color: "white",
+    fontSize: 22,
+    textAlign: "center",
+    marginTop: 80,
+  },
+
+  hellotxt: {
+    fontFamily: "Roboto",
+    fontWeight: "semibold",
+    color: "white",
+    fontSize: 30,
+    textAlign: "center",
+    marginTop: 60,
+  },
+
+  welcomemsgtxt: {
+    fontFamily: "Roboto",
+    fontWeight: "regular",
+    color: "white",
+    fontSize: 18,
+    textAlign: "center",
+    marginTop: 16,
+  },
+
+  emailstyle: {
+    flexDirection: "row",
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    marginTop: 100,
+    marginLeft: 37,
+    marginEnd: 37,
+  },
+
+  loginimg: {
+    width: 32,
+    height: 32,
+    marginBottom: 5
+  },
+
+  logintxt: {
+    fontFamily: "Roboto",
+    fontWeight: "regular",
+    fontSize: 18,
+    paddingVertical: 0,
+    marginLeft: 16,
+    marginBottom: 5
+  },
+
+  passstyle: {
+    flexDirection: "row",
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    marginTop: 35,
+    marginLeft: 37,
+    marginEnd: 37,
+  },
+});
