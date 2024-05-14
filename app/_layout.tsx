e065b0331111849aacf9efbbf27ad63cc23f7201
Jay Ruiz De Ocampo <jayruizdeocampo11503@gmail.com>
2024-05-14 22:02:16 +0800

    made the lower part

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,8 +3,13 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index"
-      options={{ headerShown: false }} />
+      <Stack.Screen
+        name="index"
+        options={{
+          headerTitle: "",
+          headerShown: false,
+        }}
+      />
     </Stack>
   );
 }
--- app/index.jsx
+++ app/index.jsx
@@ -1,35 +1,107 @@
 import React from "react";
-import { TextInput, Image, StyleSheet, View, Text } from "react-native";
+import {
+  TouchableOpacity,
+  TextInput,
+  Image,
+  StyleSheet,
+  View,
+  Text,
+  Button,
+} from "react-native";
 import email from "../assets/images/email.png";
 import pass from "../assets/images/pass.png";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.logintxt}>Login</Text>
+      <View>
+        <Text style={styles.logintxt}>Login</Text>
 
-      <Text style={styles.hellotxt}>Hello, welcome back!</Text>
-      <Text style={styles.welcomemsgtxt}>
-        Welcome back please {"\n"}sign in again
-      </Text>
-
-      <View style={styles.emailstyle}>
-        <Image source={email} style={styles.loginimg}></Image>
-        <TextInput
-          placeholderTextColor="#C4C4C4"
-          style={styles.logintxt}
-          placeholder="Email"
-        ></TextInput>
+        <Text style={styles.hellotxt}>Hello, welcome back!</Text>
+        <Text style={styles.welcomemsgtxt}>
+          Welcome back please {"\n"}sign in again
+        </Text>
       </View>
 
-      <View style={styles.passstyle}> 
-        <Image source={pass} style={styles.loginimg}></Image>
-        <TextInput
-          placeholder="Password"
-          placeholderTextColor="#C4C4C4"
-          style={styles.logintxt}
-          secureTextEntry={true}
-        ></TextInput>
+      <View>
+        <View style={styles.emailstyle}>
+          <Image source={email} style={styles.iconformat}></Image>
+          <TextInput
+            placeholderTextColor="#C4C4C4"
+            style={styles.textinputformat}
+            placeholder="Email"
+          ></TextInput>
+        </View>
+
+        <View style={styles.passstyle}>
+          <Image source={pass} style={styles.iconformat}></Image>
+          <TextInput
+            placeholder="Password"
+            placeholderTextColor="#C4C4C4"
+            style={styles.textinputformat}
+            secureTextEntry={true}
+          ></TextInput>
+        </View>
+      </View>
+
+      <View
+        style={{
+          marginTop: 35,
+        }}
+      >
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          marginTop: 21,
+        }}
+      >
+        <View style={styles.line} />
+        <Text
+          style={{
+            marginHorizontal: 10,
+            color: "#FFFFFF",
+          }}
+        >
+          or
+        </Text>
+        <View style={styles.line} />
+      </View>
+
+      <View
+        style={{
+          marginTop: 21,
+        }}
+      >
+        <TouchableOpacity style={styles.button2}>
+          <Text style={styles.buttonText2}>Facebook</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View
+        style={{
+          marginTop: 14,
+        }}
+      >
+        <TouchableOpacity style={styles.button2}>
+          <Text style={styles.buttonText2}>Gmail</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "center",
+          marginTop: 16,
+        }}
+      >
+        <Text style={styles.noAcc}>Don't have an account? </Text>
+        <Text style={styles.regNow}>Register now</Text>
       </View>
     </View>
   );
@@ -48,7 +120,7 @@ const styles = StyleSheet.create({
     color: "white",
     fontSize: 22,
     textAlign: "center",
-    marginTop: 80,
+    marginTop: 75,
   },
 
   hellotxt: {
@@ -71,34 +143,88 @@ const styles = StyleSheet.create({
 
   emailstyle: {
     flexDirection: "row",
-    borderBottomColor: 'black',
+    borderBottomColor: "black",
     borderBottomWidth: 1,
     marginTop: 100,
     marginLeft: 37,
     marginEnd: 37,
   },
 
-  loginimg: {
+  iconformat: {
     width: 32,
     height: 32,
-    marginBottom: 5
+    marginBottom: 5,
   },
 
-  logintxt: {
+  textinputformat: {
     fontFamily: "Roboto",
     fontWeight: "regular",
     fontSize: 18,
     paddingVertical: 0,
     marginLeft: 16,
-    marginBottom: 5
+    marginBottom: 5,
   },
 
   passstyle: {
     flexDirection: "row",
-    borderBottomColor: 'black',
+    borderBottomColor: "black",
     borderBottomWidth: 1,
     marginTop: 35,
     marginLeft: 37,
     marginEnd: 37,
+  },
+
+  button: {
+    backgroundColor: "#FFFFFF",
+    paddingVertical: 12,
+    paddingHorizontal: 25,
+    borderRadius: 25,
+    alignItems: "center",
+    justifyContent: "center",
+    marginStart: 36,
+    marginEnd: 37,
+  },
+
+  button2: {
+    backgroundColor: "#11325C",
+    paddingVertical: 12,
+    paddingHorizontal: 25,
+    borderRadius: 25,
+    alignItems: "center",
+    justifyContent: "center",
+    marginStart: 16,
+    marginEnd: 17,
+  },
+
+  buttonText: {
+    color: "#00224F",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+
+  buttonText2: {
+    color: "#FFFFFF",
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+
+  line: {
+    flex: 1,
+    height: 1,
+    backgroundColor: "#000",
+  },
+
+  noAcc: {
+    fontFamily: "Roboto",
+    fontWeight: "regular",
+    fontSize: 16,
+    color: "#999999",
+  },
+
+  regNow: {
+    fontFamily: "Roboto",
+    fontWeight: "regular",
+    fontSize: 16,
+    color: "#0F47A5",
   },
 });
